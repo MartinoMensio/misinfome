01bd000f009c30029a27dd3897318df2c82fa0ea
Martino Mensio <martino.mensio@open.ac.uk>
2023-04-26 19:22:18 +0200

    latest

--- scripts/init_web_db.sh
+++ scripts/init_web_db.sh
@@ -6,7 +6,7 @@ set -e
 curl -X POST http://localhost:20000/misinfo/api/data/update \
 -H 'Content-Type: application/json' \
   -d '{
-  "date": "2023_04_23"
+  "date": "2023_04_25"
 }'
 # update other data
 curl -X POST http://localhost:20300/origins/
