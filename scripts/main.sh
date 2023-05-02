9772f7044c8a8b26c530b9734a019aae6eb7e2b7
Martino Mensio <martino.mensio@open.ac.uk>
2023-05-02 18:35:03 +0200

    non-interactive mode by default

--- scripts/main.sh
+++ scripts/main.sh
@@ -2,11 +2,13 @@
 set -e
 
 help=0
+interactive=0
 
 while getopts d:h flag
 do
     case "${flag}" in
         h) help=1;;
+        i) interactive=1;;
         d) case "${OPTARG}" in
             misinfome-backend) export BACKEND_TAG=dev;;
             credibility) export CREDIBILITY_TAG=dev;;
@@ -18,17 +20,10 @@ do
 done
 
 if [ "$help" = "1" ]; then
-    echo "Usage: $0 [-m] [-r] [-d misinfome-backend|credibility|twitter-connector|claimreview-collector] [-h]"
-    echo "  -m: run with source code mapped as volume"
-    echo "  -r: remove containers"
+    echo "Usage: $0 [-i] [-d misinfome-backend|credibility|twitter-connector|claimreview-collector] [-h]"
+    echo "  -i: interactive mode"
     echo "  -d: which submodule is using dev tag (misinfome-backend, credibility, twitter-connector, claimreview-collector, all)"
     echo "  -h: help"
-    exit 0
-fi
-
-if [ "$remove" = "1" ]; then
-    echo "Removing containers"
-    docker rm -f mm35626_mongo mm35626_redis mm35626_misinfo_server mm35626_claimreview_collector_light mm35626_credibility mm35626_twitter_connector
     exit 0
 fi
 
@@ -47,23 +42,39 @@ if [ "$COMMAND" = "start.web" ]; then
     ./scripts/download_frontend.sh
     export COMPOSE_PROJECT_NAME=mm35626-web
     docker-compose -f docker-compose.web.yml pull
-    docker-compose -f docker-compose.web.yml up
+    if [ "$interactive" = "1" ]; then
+        docker-compose -f docker-compose.web.yml up
+    else
+        docker-compose -f docker-compose.web.yml up --detach
+    fi
 elif [ "$COMMAND" = "start.web.dev" ]; then
     echo "Starting web services dev"
     ./scripts/download_frontend.sh -m
     export COMPOSE_PROJECT_NAME=mm35626-web
     docker-compose -f docker-compose.web.yml -f docker-compose.web.dev.yml pull
-    docker-compose -f docker-compose.web.yml -f docker-compose.web.dev.yml up
+    if [ "$interactive" = "1" ]; then
+        docker-compose -f docker-compose.web.yml -f docker-compose.web.dev.yml up
+    else
+        docker-compose -f docker-compose.web.yml -f docker-compose.web.dev.yml up --detach
+    fi
 elif [ "$COMMAND" = "start.collector" ]; then
     echo "Starting collector services"
     export COMPOSE_PROJECT_NAME=mm35626-collector
     docker-compose -f docker-compose.collector.yml pull
-    docker-compose -f docker-compose.collector.yml up
+    if [ "$interactive" = "1" ]; then
+        docker-compose -f docker-compose.collector.yml up
+    else
+        docker-compose -f docker-compose.collector.yml --detach
+    fi
 elif [ "$COMMAND" = "start.collector.dev" ]; then
     echo "Starting collector services dev"
     export COMPOSE_PROJECT_NAME=mm35626-collector
     docker-compose -f docker-compose.collector.yml -f docker-compose.collector.yml pull
-    docker-compose -f docker-compose.collector.yml -f docker-compose.collector.yml up
+    if [ "$interactive" = "1" ]; then
+        docker-compose -f docker-compose.collector.yml -f docker-compose.collector.yml up up
+    else
+        docker-compose -f docker-compose.collector.yml -f docker-compose.collector.yml up --detach
+    fi
 else
     echo "Unknown command: $COMMAND, please set env variable COMMAND to one of start.web, start.web.dev, start.collector, start.collector.dev"
     exit 1
